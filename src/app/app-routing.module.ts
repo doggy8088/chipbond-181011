39ba247db57675625c48937ab9bad42b65226a42
wellwind <wellwind@mail2000.com.tw>
2018-10-12 10:25:02 +0800

    練習使用 ion-grid

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ const routes: Routes = [
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'form', loadChildren: './demo/form/form.module#FormPageModule' },
   { path: 'tabs', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'grid', loadChildren: './grid/grid.module#GridPageModule' },
 ];
 
 @NgModule({
